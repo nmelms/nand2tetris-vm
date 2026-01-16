78cf9ca7b832cd4e6b6f6a6b434e6af6f8ec95a3
Nick Melms <nmelms92@gmail.com>
2026-01-15 19:04:47 -0800

    create command type function

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,10 @@
-
-
-pub struct Parser{
+pub struct Parser {
     lines: Vec<String>,
     i: usize,
     current_cmd: String,
 }
 
-pub enum CommandType{
+pub enum CommandType {
     ARITHMETIC,
     PUSH,
     POP,
@@ -20,29 +18,41 @@ pub enum CommandType{
 }
 
 impl Parser {
-    
     pub fn hasMoreLines(&self) -> bool {
         if self.i < self.lines.len() {
             true
-        }else{
+        } else {
             false
         }
     }
 
-
-    pub fn advance(&mut self){
+    pub fn advance(&mut self) {
         self.current_cmd = self.lines[self.i].clone();
         self.i += 1;
     }
 
-    pub fn command_type(&self){
-        let split_command: Vec<&str> = self.current_cmd.spit(" ");
+    pub fn command_type(&self) {
+        let split_command: Vec<&str> = self.current_cmd.split(" ").collect();
 
-        match split_command[0]{
+        match split_command[0] {
             "add" => CommandType::ARITHMETIC,
+            "sub" => CommandType::ARITHMETIC,
+            "neg" => CommandType::ARITHMETIC,
+            "eq" => CommandType::ARITHMETIC,
+            "gt" => CommandType::ARITHMETIC,
+            "lt" => CommandType::ARITHMETIC,
+            "and" => CommandType::ARITHMETIC,
+            "or" => CommandType::ARITHMETIC,
+            "not" => CommandType::ARITHMETIC,
+            "push" => CommandType::PUSH,
+            "pop" => CommandType::POP,
+            "label" => CommandType::LABEL,
+            "goto" => CommandType::GOTO,
+            "if-goto" => CommandType::IF,
+            "function" => CommandType::FUNCTION,
+            "return" => CommandType::RETURN,
+            "call" => CommandType::CALL,
             _ => CommandType::NONE,
         };
-    }   
-
-
+    }
 }
